a62d1b1306321857bdc77bde796dbd0cde6df670
Dan <92699502+thedvo@users.noreply.github.com>
2026-03-30 17:27:00 -0700

    Create initial Swift project files

---
+++ MotionIQ/MotionIQ/MotionIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionIQApp.swift
+//  MotionIQ
+//
+//  Created by Dan Vo on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct MotionIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
